64cd3360b4793c5ae00145f4befcd20fe77539c9
Florian Wahl <41266220+wahlflo@users.noreply.github.com>
2024-06-26 18:00:57 +0200

    improved detection of AWS - Secret Key

--- pkg/detectors/regex/patterns/tokens/AWS.go
+++ pkg/detectors/regex/patterns/tokens/AWS.go
@@ -14,6 +14,11 @@ func AwsAccessKeyId() patterns.Pattern {
 
 func AwsSecretKey() patterns.Pattern {
 	pattern := patterns.NewSimpleRegexPattern("AWS - Secret Key", interfaces.FindingPriorityHigh)
-	pattern.AddRegexPattern(regexp.MustCompile("(?m)(\\W|^)([0-9a-zA-Z/+]{40})(\\W|$)"), 2)
+	// the AWS - Secret Key is hard to match as it follows no specific pattern
+	// to reduce false positives the value has to be surrounded by " or '
+	// or on the right side of an assignment operator : or =
+	pattern.AddRegexPattern(regexp.MustCompile("(?m)\"([0-9a-zA-Z/+]{40})\""), 1)
+	pattern.AddRegexPattern(regexp.MustCompile("(?m)'([0-9a-zA-Z/+]{40})'"), 1)
+	pattern.AddRegexPattern(regexp.MustCompile("(?m)[=|:]\\s+([0-9a-zA-Z/+]{40})(\\W|$)"), 1)
 	return pattern
 }
--- pkg/detectors/regex/patterns/tokens/AWS_test.go
+++ pkg/detectors/regex/patterns/tokens/AWS_test.go
@@ -112,3 +112,26 @@ AWS_ACCESS_KEY_ID=asASIAIOSFODNN7EXAMPLE
 	matches := pattern.GetMatches("", []byte(testContent))
 	require.Equal(t, 0, len(matches))
 }
+
+func Test_AwsSecretKey_positive(t *testing.T) {
+	testContent := `
+"123456789+123456789+123456789+123456789+"
+'0000000000000000000000000000000000000000'
+= 8d75008285babd33fd02e4160a6b1232d9521566
+: 8d75008285babd33fd02e4160a6b1232d9521566
+`
+	pattern := AwsSecretKey()
+	matches := pattern.GetMatches("", []byte(testContent))
+	require.Equal(t, 4, len(matches))
+}
+
+func Test_AwsSecretKey_negative(t *testing.T) {
+	testContent := `
+123456789+123456789+123456789+123456789+
+0000000000000000000000000000000000000000
+8d75008285babd33fd02e4160a6b1232d9521566
+`
+	pattern := AwsSecretKey()
+	matches := pattern.GetMatches("", []byte(testContent))
+	require.Equal(t, 0, len(matches))
+}
